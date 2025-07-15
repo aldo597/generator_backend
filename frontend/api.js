c0f393c9690c9c4eed7ef01ec8fbd4d2c892fbf7
jh8342 <josi.huebner@gmx.de>
2025-07-15 11:48:30 +0200

    z. B. Setze Vite base Pfad für GitHub Pages

--- frontend/api.js
+++ frontend/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-// Create an instance of axios with the base URL
+// Create an instance of axios with the base URLgit
 const api = axios.create({
   //baseURL: "http://localhost:8001"
   baseURL: "https://aldo597.github.io/generator/"
