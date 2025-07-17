08607c56e226a6c627bcdb9f917e35b539cae90e
aldo597 <cataldo.caruso@ep.europa.eu>
2025-07-17 09:30:05 +0200

    Ordnerstruktur korrigiert

--- frontend/api.js
+++ frontend/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "http://localhost:8001"
+  //baseURL: "http://localhost:8001"
+  baseURL: "https://generatorbackend-production.up.railway.app/"
 });
 
 export default api;
