35777c9fe2dc1ebd11a6b6ccf9c063d2a09b4992
aldo597 <cataldo.caruso@ep.europa.eu>
2025-12-09 09:53:51 +0100

    chasnge source

--- frontend/api.js
+++ frontend/api.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-//const BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
-const BASE = "https://railway.com?referralCode=gKJ-9H"; 
+const BASE = import.meta.env.VITE_API_URL 
+           || "https://<dein-projekt>.railway.app";
 
 const api = axios.create({
   baseURL: BASE,
   timeout: 30000,
-  // ggf. headers: { 'Content-Type': 'application/json' }
 });
 
 export default api;
