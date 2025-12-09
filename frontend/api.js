5758eaddd63e7be85cb5c2069667707768435e91
aldo597 <cataldo.caruso@ep.europa.eu>
2025-12-09 22:57:29 +0100

    chasnge source

--- frontend/api.js
+++ frontend/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 // Base URL: Railway Deployment
-const BASE = import.meta.env.VITE_API_URL 
-           || "https://generatorbackend-production-6bce.up.railway.app";
+const BASE = "https://generatorbackend-production-6bce.up.railway.app";
 
 const api = axios.create({
   baseURL: BASE,    // Axios baut automatisch die vollständige URL
